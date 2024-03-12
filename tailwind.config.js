8f4af945c459b8bc184534fd3dc58c5065e1c4b8
Oğuz Can Yılmazlar <yilmazlaroguzcan@gmail.com>
2024-03-12 21:37:02 +0300

    do stuff

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,13 @@
 /** @type {import('tailwindcss').Config} */
+
+const colors = require('tailwindcss/colors')
+
+
 module.exports = {
   content: ["./index.html", "./goggle/index.html"],
   theme: {
     colors: {
-      'blue': '#1fb6ff',
-      'purple': '#7e5bef',
-      'pink': '#ff49db',
-      'orange': '#ff7849',
-      'green': '#13ce66',
-      'yellow': '#ffc82c',
-      'gray-dark': '#273444',
-      'gray': '#8492a6',
-      'gray-light': '#d3dce6',
+      ...colors
     },
     fontFamily: {
       sans: ['Graphik', 'sans-serif'],
